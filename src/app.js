56dd28f9025ceadf597e398b7f6ba550a6ffd5fa
devante.moore <djm5696@gmail.com>
2022-09-03 12:29:45 -0500

    WIP: make calculator work

---
+++ src/app.js
@@ -0,0 +1,13 @@
+console.log("hello from app.js")
+
+export function calculate() {
+    validate();
+    var homePrice = document.getElementById("homePrice")
+    var downPayment = document.getElementById("downPayment")
+    var interestRate = document.getElementById("interestRate")
+    var loanDuration = document.getElementById("loanDuration")
+}
+
+function validate(){
+    
+}
