72428d90d0f8ec9cca05f5e19a8dcee6cb520a41
devante.moore <djm5696@gmail.com>
2022-09-06 11:35:06 -0500

    clean up code. added validation and styling

--- src/app.js
+++ src/app.js
@@ -1,25 +1,33 @@
 console.log("hello from app.js")
 
-function calculate() {
-    //TODO: clean up function/implement validation
-    validate();
+function calculate() {    
     var homePrice = document.getElementById("homePrice").value
     var downPayment = document.getElementById("downPayment").value
     var interestRate = document.getElementById("interestRate").value
     var loanDuration = document.getElementById("loanDuration").value
+
+    var validation = document.getElementById("validation")
+    if (homePrice=="" || downPayment=="" || interestRate=="" || loanDuration==0)
+    {
+        validation.style.color = "red";
+        validation.innerText = "Fields cannot be left blank"
+        return;
+    }
+    if (homePrice < downPayment){
+        validation.style.color = "red";
+        validation.innerText = "How your down payment greater than the home price ???"
+    }
+
+
     var displayCalculation = document.getElementById("monthlyPayment")
-    // console.log(homePrice, downPayment, interestRate, loanDuration)
+
     // Monthly Payment = Principal * [ (i/12mo)(i + 1)^LD]/[(1 + i)^n - 1]
     let monthlyInterest = (interestRate/100)/12
     let loanDurationInMonths = loanDuration*12
     let loanAmount = homePrice - downPayment
     var monthlyPayment = loanAmount * ((monthlyInterest*(monthlyInterest + 1)**loanDurationInMonths)/((monthlyInterest + 1)**(loanDurationInMonths) - 1));
-    //var monthlyPayment = ((monthlyInterest + 1)^(120) - 1)
-    console.log(monthlyPayment)
 
     displayCalculation.innerText = monthlyPayment;
 }
 
-function validate(){
 
-}
