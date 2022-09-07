d7d920929cb63c5b651840b1d9947f103191a1bb
devante.moore <djm5696@gmail.com>
2022-09-07 16:51:48 -0500

    styled principal+interest payment result and label

--- src/app.js
+++ src/app.js
@@ -19,15 +19,43 @@ function calculate() {
     }
 
 
-    var displayCalculation = document.getElementById("monthlyPayment")
+    var displayCalculation = document.getElementById("principalPlusInterestValue")
 
     // Monthly Payment = Principal * [ (i/12mo)(i + 1)^LD]/[(1 + i)^n - 1]
     let monthlyInterest = (interestRate/100)/12
     let loanDurationInMonths = loanDuration*12
     let loanAmount = homePrice - downPayment
-    var monthlyPayment = loanAmount * ((monthlyInterest*(monthlyInterest + 1)**loanDurationInMonths)/((monthlyInterest + 1)**(loanDurationInMonths) - 1));
+    var principalPlusInterest = loanAmount * ((monthlyInterest*(monthlyInterest + 1)**loanDurationInMonths)/((monthlyInterest + 1)**(loanDurationInMonths) - 1));
 
-    displayCalculation.innerText = monthlyPayment;
+    displayCalculation.innerText += " " + currencyFormat(principalPlusInterest.toFixed(2));
+}
+
+function currencyFormat(number){
+    if(number==null) return;
+    let formattedNumber = number.toString().split('');
+    let numberOfDigits = formattedNumber.length - 3; //subtract ".00"
+    let numberOfCommas = Math.floor(numberOfDigits / 3);
+    numberOfCommas = numberOfDigits % 3 == 0 ? numberOfCommas - 1 : numberOfCommas; // comma may not be necessary
+    if (numberOfDigits <= 3) return number;
+    switch (numberOfCommas){
+        case 0:
+            return formattedNumber.join('');
+        case 1:
+            formattedNumber.splice(1, 0, ',');
+            return formattedNumber.join('');
+            
+        case 2:
+            formattedNumber.splice(1, 0, ',')
+            formattedNumber.splice(5, 0, ',')
+            return formattedNumber.join('');
+        case 3:
+            formattedNumber.splice(1, 0, ',')
+            formattedNumber.splice(5, 0, ',')
+            formattedNumber.splice(5, 0, ',')
+            return formattedNumber.join('');
+        default:
+            return;
+    }
 }
 
 
