3e7cc61e6dfcb817777803a326ec4dc90c04d0e5
devante.moore <djm5696@gmail.com>
2022-09-09 13:25:19 -0500

    added event listeners for input tags

--- src/app.js
+++ src/app.js
@@ -1,28 +1,46 @@
 console.log("hello from app.js")
 
-const additionalOptionsList = document.querySelector('#additionalOptionsList')
+// wrap in initializer function? document.ready
+
 const additionalOptionsButton = document.querySelector('#additionalOptionsButton')
+const additionalOptionsList = document.querySelector('#additionalOptionsList')
 const chevron = document.querySelector('.icon.icon-chevron-down')
+const downPayment = document.getElementById("downPayment")
+const homePrice = document.getElementById("homePrice")
+const interestRate = document.getElementById("interestRate")
+const loanDuration = document.getElementById("loanDuration")
+const percentage = document.querySelector('#percentage')
 
 additionalOptionsButton.addEventListener('click', function(){
     additionalOptionsList.classList.toggle('is-active')
     chevron.classList.toggle('rotate-180')
 })
+downPayment.addEventListener('change', function() {
+    percentage.value = ((downPayment.value / homePrice.value) * 100).toFixed(1);
+})
+homePrice.addEventListener('change', function(){
+    downPayment.value = (homePrice.value * (percentage.value/100)).toFixed(0);
+})
+percentage.addEventListener('change', function(){
+    downPayment.value = (homePrice.value * (percentage.value/100)).toFixed(0);
+})
 
-function calculate() {    
-    var homePrice = document.getElementById("homePrice").value
-    var downPayment = document.getElementById("downPayment").value
-    var interestRate = document.getElementById("interestRate").value
-    var loanDuration = document.getElementById("loanDuration").value
+
+setDefaultValues();
+calculate();
+
+
+
+function calculate() {
 
     var validation = document.getElementById("validation")
-    if (homePrice=="" || downPayment=="" || interestRate=="" || loanDuration==0)
+    if (homePrice.value=="" || downPayment.value=="" || interestRate.value=="" || loanDuration.value==0)
     {
         validation.style.color = "red";
         validation.innerText = "Fields cannot be left blank"
         return;
     }
-    if (parseFloat(homePrice) < parseFloat(downPayment)){
+    if (parseFloat(homePrice.value) < parseFloat(downPayment.value)){
         validation.style.color = "red";
         validation.innerText = "How your down payment greater than the home price ???"
     }
@@ -31,12 +49,12 @@ function calculate() {
     var displayCalculation = document.getElementById("principalPlusInterestValue")
 
     // Monthly Payment = Principal * [ (i/12mo)(i + 1)^LD]/[(1 + i)^n - 1]
-    let monthlyInterest = (interestRate/100)/12
-    let loanDurationInMonths = loanDuration*12
-    let loanAmount = homePrice - downPayment
+    let monthlyInterest = (interestRate.value/100)/12
+    let loanDurationInMonths = loanDuration.value*12
+    let loanAmount = homePrice.value - downPayment.value
     var principalPlusInterest = loanAmount * ((monthlyInterest*(monthlyInterest + 1)**loanDurationInMonths)/((monthlyInterest + 1)**(loanDurationInMonths) - 1));
 
-    displayCalculation.innerText += "$ " + currencyFormat(principalPlusInterest.toFixed(2));
+    displayCalculation.innerText = "$ " + currencyFormat(principalPlusInterest.toFixed(2));
 }
 
 function currencyFormat(number){
@@ -67,4 +85,9 @@ function currencyFormat(number){
     }
 }
 
-
+function setDefaultValues(){
+    homePrice.defaultValue = 300000;
+    downPayment.defaultValue = 60000;
+    interestRate.defaultValue = 6.02; // set to national average
+    loanDuration.defaultValue = 30;
+}
